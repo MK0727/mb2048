f9409814efff5cac5ec902c75032061cb0f86e57
AICK-猫猫 <104549811+MK0727@users.noreply.github.com>
2025-06-19 00:23:46 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// 数据库配置
+$host = "localhost";
+$dbname = "bread2048";
+$user = "bread2048";
+$pass = "mx123456";
+
+// 确保此文件不被公开访问
+if (isset($_SERVER['HTTP_HOST'])) {
+    die('此文件不能直接访问');
+}
+?>
